fd6134978f954d63a8945da995097627fc6e57da
Kimberly <kimolanga123@gmail.com>
2022-05-27 15:27:05 +0300

    add date converter function

--- pictures/views.py
+++ pictures/views.py
@@ -8,12 +8,22 @@ def welcome(request):
 
 def post_of_day(request):
     date = dt.date.today()
+    day = convert_dates(date)
     html = f'''
        <html>
             <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
+                <h1>Post for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html> 
             '''
     return HttpResponse(html)
 
+def convert_dates(dates):
+    #Function that gets that weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    #Returning the actual of the week
+    day = days[day_number]
+    return day
