68584dd005c382d65f954a37d3ec32907b4bc9f1
Kimberly <kimolanga123@gmail.com>
2022-05-27 15:18:29 +0300

    add return method

--- pictures/urls.py
+++ pictures/urls.py
@@ -4,4 +4,5 @@ from . import views
 #create paths
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
+    path('today/',views.post_of_day,name = 'postToday'),
 ]
--- pictures/views.py
+++ pictures/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 # Create your views here.
 def welcome(request):
     return HttpResponse('welcome to the art gallery')
+
+def post_of_day(request):
+    date = dt.date.today()
+    html = f'''
+       <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html> 
+            '''
+    return HttpResponse(html)
+
