c99e443b3b34e0cc185ee3f024c06f73d315e103
Kimberly <kimolanga123@gmail.com>
2022-05-27 16:25:27 +0300

    add navbar.html

--- gallery/settings.py
+++ gallery/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pictures.apps.PicturesConfig',
+    'bootstrap',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
