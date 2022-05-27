95580d34bb23554a35a1e29803996331ef938f5e
Kimberly <kimolanga123@gmail.com>
2022-05-27 15:58:17 +0300

    add template folder

--- pictures/urls.py
+++ pictures/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
     path('today/',views.post_of_day,name = 'postToday'),
+    path('archive/({4}-{2}-{2})/',views.past_days_post,name= 'pastPost')
 ]
--- pictures/views.py
+++ pictures/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 import datetime as dt
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 # Create your views here.
 def welcome(request):
@@ -27,3 +27,21 @@ def convert_dates(dates):
     #Returning the actual of the week
     day = days[day_number]
     return day
+
+def past_days_post(request,past_date):
+    try:
+        #Converts data from the string url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
