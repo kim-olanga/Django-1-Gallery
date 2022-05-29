882f5dcd46ccdc548c2f758c1f028ebbda3ba98c
Kimberly <kimolanga123@gmail.com>
2022-05-29 12:21:25 +0300

    add if && else statements

--- pictures/views.py
+++ pictures/views.py
@@ -17,6 +17,26 @@ def viewPhoto(request, pk):
 def addPhoto(request):
     return render(request, 'add.html')
 
+def search(request):
+    categories=Category.objects.all()
+    locations=Location.objects.all()
+    if 'search' in request.GET and request.GET["search"]:
+        term_of_search = request.GET.get("search")
+        searched_images = Image.search_image(term_of_search)
+        print("___________________________________________")
+        print(searched_images)
+        message = f"{term_of_search}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images, 'categories':categories, 'locations':locations})
+
+    else:
+        message = "Kindly input your search request"
+        return render(request, 'search.html',{"message":message})
+      
+def get_location(request, location_id):
+    location_images=Image.filter_by_location(location_id)
+    return render(request, 'location.html', {'images':location_images})
+
 def post_of_day(request):
     date = dt.date.today()
     
