916cb59237128099b7789df4422c6506a1ba8207
Kimberly <kimolanga123@gmail.com>
2022-05-29 22:08:51 +0300

    add procfile

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 #create paths
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
-    path('photo/<str:pk>',views.viewPhoto,name = 'photo'),
-    path('add/',views.addPhoto,name = 'add'),
-    path('today/',views.post_of_day,name = 'postToday'),
-    path('archive/({4}-{2}-{2})/',views.past_days_post,name= 'pastPost')
+    path('search_results/', views.search, name ='search_results') ,
+    path('location_results/<int:location_id>', views.get_location, name="location")
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
