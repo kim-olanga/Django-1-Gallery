6a57abbb91e22e56124ab09053b1e6c0025b8199
Kimberly <kimolanga123@gmail.com>
2022-05-29 12:34:30 +0300

    add photo uploader through pillow

--- pictures/tests.py
+++ pictures/tests.py
@@ -16,3 +16,18 @@ class CategoryTestClass(TestCase):
         self.new_category.save_category()
         categories = Category.objects.all()
         self.assertTrue(len(categories) > 0)
+
+class LocationTestClass(TestCase):
+    #set up method 
+    def setUp(self):
+        self.location=Location(location = "Location1")
+
+    #instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location,Location))
+
+    #save method
+    def test_save_method(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
